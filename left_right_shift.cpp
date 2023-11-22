b07c5f173c8cd5487184f5d433b470f64cbe34b5
erkinovalim <alimerkinov43@gmail.com>
2023-11-22 23:41:56 +0600

    Binary left and right shift

---
+++ left_right_shift.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    int a = 5;
+
+    a = a << 2; // shift binary numbers in the a variable to the left by 2
+
+    // binary left shift (x<<y) does this operation (x * 2^y)
+
+    cout << a << endl;
+
+    a = a >> 2;
+
+    // binary right shift (x<<y) does this operation (x / 2^y)
+
+    cout << a << endl;
+
+    return 0;
+}
