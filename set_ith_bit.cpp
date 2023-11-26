935a54e1b135d4a873cd5dabde54306ee9e4a1cd
erkinovalim <alimerkinov43@gmail.com>
2023-11-26 23:53:13 +0600

    Update set_ith_bit.cpp

--- set_ith_bit.cpp
+++ set_ith_bit.cpp
@@ -1,1 +1,23 @@
-#
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void set_ith_bit(int &n, int i)
+{
+    int mask = (1<<i);
+
+    n = (n|mask);
+}
+
+int main()
+{
+    int n = 5;
+
+    int i;
+
+    cin>>i;
+
+    set_ith_bit(n,i);
+
+    cout<<n;
+}
