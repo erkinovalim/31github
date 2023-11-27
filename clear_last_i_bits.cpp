bbc7be266e1a78cb10d1fec51771a46fc7b0ef98
Alim <131399881+erkinovalim@users.noreply.github.com>
2023-11-27 23:49:12 +0600

    Create clear_last_i_bits.cpp

---
+++ clear_last_i_bits.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void clearLastIBits(int &n, int i)
+{
+    int mask = ~(-1 << i);
+
+    n = n & mask;
+}
+
+int main()
+{
+  int n = 5;
+
+  int i = 2;
+
+  clearLastIBits(n , i);
+
+  cout<<n;
+}
