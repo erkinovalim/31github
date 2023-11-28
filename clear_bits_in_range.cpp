e997f6481dcfc7408be7ac2f89bff0d91e11adbf
Alim <131399881+erkinovalim@users.noreply.github.com>
2023-11-28 23:52:40 +0600

    Create clear_bits_in_range.cpp

---
+++ clear_bits_in_range.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void clearBitsInRange(int &n, int i, int j)
+{
+  int a = ~(0)<<j + 1;
+
+  int b = (1<<i) - 1;
+
+  int mask = a|b;
+
+  return n&mask;
+}
+
+int main()
+{
+int n = 5;
+
+int i = 5;
+
+int j = 2;
+
+clearBitsInRange(n,i,j);
+
+cout<<n;
+}
